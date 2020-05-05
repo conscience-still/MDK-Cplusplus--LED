4e82963798c20bf828b10e07668475eacbea924f
ConscienceRemain <631579741@qq.com>
2020-05-05 10:56:01 +0800

    little change

--- MDK-ARM/c++test/main.d
+++ MDK-ARM/c++test/main.d
@@ -1,4 +1,4 @@
-c++test\main.o: ..\Src\main.c
+c++test\main.o: ..\Src\main.cpp
 c++test\main.o: ../Inc/main.h
 c++test\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_crs.h
 c++test\main.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h
